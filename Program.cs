ab4c684a7052edc7c8e49e50e1227a17d8d29617
Max <0665232@mail.ru>
2022-10-27 22:32:16 +0300

    Решена 50

--- Program.cs
+++ Program.cs
@@ -23,3 +23,39 @@ m = 3, n = 4.
 // }
 
 
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+[1,7] -> такого элемента в массиве нет*/
+
+// Console.WriteLine("Введите количество столбцов");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[m, n];
+ 
+// for (int i = 0; i < array.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//         array [i, j] = Convert.ToInt32(new Random().Next(1,100));  
+// }
+ 
+// for (int i = 0; i < array.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//         Console.Write(array[i,j] + "\t ");
+//         Console.WriteLine();
+// }
+ 
+//  Console.WriteLine("Введите индекс числа");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  int b = Convert.ToInt32(Console.ReadLine());
+// if (a>m && b>n)
+//  Console.WriteLine("Вы вышли за пределы массива");
+//  else
+//  {
+//  Console.WriteLine(array[a,b]);
+//  }
