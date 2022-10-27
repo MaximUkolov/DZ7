247e1ba48f3ee085b96eba9be5cbb88d5361e857
Max <0665232@mail.ru>
2022-10-27 23:05:29 +0300

    Решена на половину 52

--- Program.cs
+++ Program.cs
@@ -59,3 +59,34 @@ m = 3, n = 4.
 //  {
 //  Console.WriteLine(array[a,b]);
 //  }
+
+
+
+/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(1,100));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t ");
+        Console.WriteLine();
+}
+
+
+// пока не сообразил как доделать
